70e89c08201c774f8ffc5548ccf74bf1aa619b5a
Jang-DongHo <donghoo18@naver.com>
2023-07-13 10:58:28 +0900

    [Feat] People 페이지 작업 마무리

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -25,7 +25,7 @@ window.addEventListener("resize", function () {
   if (windowWidth >= 1024) {
     document.body.style.fontSize = "16px"; // 가로 크기가 1024px 이상일 때
   } else if (windowWidth >= 768) {
-    document.body.style.fontSize = "12px"; // 가로 크기가 768px 이상일 때
+    document.body.style.fontSize = "14px"; // 가로 크기가 768px 이상일 때
   } else {
     document.body.style.fontSize = "12px"; // 가로 크기가 767px 이하일 때
   }
