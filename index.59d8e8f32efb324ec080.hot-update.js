f88f70f065915d173b0813070bfd2180ea1c384d
Jang-DongHo <donghoo18@naver.com>
2023-07-15 23:22:30 +0900

    Updates

---
+++ index.59d8e8f32efb324ec080.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateneeds_lab_web"]("index",{
+
+/***/ "./src/assets/styles/styles.scss":
+/*!***************************************!*\
+  !*** ./src/assets/styles/styles.scss ***!
+  \***************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nexpected \\\"{\\\".\\n  ╷\\n8 │         h3.title 2020~\\n  │                       ^\\n  ╵\\n  src/assets/styles/pages/publication.scss 8:23  @import\\n  src/assets/styles/styles.scss 22:9             root stylesheet\\n    at tryRunOrWebpackError (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5067:12)\\n    at __webpack_require__ (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5024:18)\\n    at /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5095:20\\n    at symbolIterator (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5002:43\\n    at symbolIterator (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:3482:9)\\n    at timesSync (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:2297:7)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nexpected \\\"{\\\".\\n  ╷\\n8 │         h3.title 2020~\\n  │                       ^\\n  ╵\\n  src/assets/styles/pages/publication.scss 8:23  @import\\n  src/assets/styles/styles.scss 22:9             root stylesheet\\n    at Object.<anonymous> (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/css-loader/dist/cjs.js!/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/sass-loader/dist/cjs.js!/Users/dongho/Desktop/Github/NEEDs-Lab-Web/src/assets/styles/styles.scss:1:7)\\n    at /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:452:10\\n    at Hook.eval [as call] (eval at create (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5069:39\\n    at tryRunOrWebpackError (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5067:12)\\n    at __webpack_require__ (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5024:18)\\n    at /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/webpack/lib/Compilation.js:5095:20\\n    at symbolIterator (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/neo-async/async.js:3527:9)\\n\\nGenerated code for /Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/css-loader/dist/cjs.js!/Users/dongho/Desktop/Github/NEEDs-Lab-Web/node_modules/sass-loader/dist/cjs.js!/Users/dongho/Desktop/Github/NEEDs-Lab-Web/src/assets/styles/styles.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nexpected \\\\\\\"{\\\\\\\".\\\\n  ╷\\\\n8 │         h3.title 2020~\\\\n  │                       ^\\\\n  ╵\\\\n  src/assets/styles/pages/publication.scss 8:23  @import\\\\n  src/assets/styles/styles.scss 22:9             root stylesheet\\\");\");\n\n//# sourceURL=webpack://needs-lab-web/./src/assets/styles/styles.scss?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("7a8939507e0b05e472d7")
+/******/ })();
+/******/ 
+/******/ }
+);
