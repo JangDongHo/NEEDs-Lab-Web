c4df19664b01bca14bdcffb95159cfbd7764a341
Jang-DongHo <donghoo18@naver.com>
2023-07-26 11:44:04 +0900

    [Feat] 갤러리 페이지 작업

--- src/assets/scripts/pages/gallery.js
+++ src/assets/scripts/pages/gallery.js
@@ -1,27 +1,89 @@
-// Assuming 'wr_id' is a query parameter with the gallery ID, you can get it like this:
+import { getExcelData } from "../getData";
+const sheetName = "Gallery";
+
 const params = new URLSearchParams(window.location.search);
 const galleryId = params.get("title");
 const galleryContainer = document.querySelector(".gallery__list-container");
 const boardContainer = document.querySelector(".board__list-container");
 
-// 갤러리
-if (galleryId) {
-  boardContainer.style.display = "block";
-  galleryContainer.style.display = "none";
-}
-// 갤러리 리스트
-else {
-  const galleryLists = galleryContainer.querySelectorAll(".gallery__list");
+const drawGallerys = (gallery) => {
+  let { ImageID, Title, Date } = gallery;
+  ImageID = ImageID.split(",");
 
-  // 클릭 이벤트 핸들러
-  galleryLists.forEach((galleryList) => {
-    galleryList.addEventListener("click", (e) => {
-      const targetGalleryList = e.target.closest(".gallery__list");
-      const titleElement = targetGalleryList.querySelector(".gallery__list__title");
-      const title = titleElement.innerText.replace(/\s+/g, "-");
+  const galleryList = document.createElement("a");
+  galleryList.classList.add("gallery__list");
+  const titleLink = Title.replace(/\s+/g, "-");
+  galleryList.href = `./gallery?title=${titleLink}`;
 
-      // 클릭한 게시글의 wr_id를 가지고 detail 페이지로 이동
-      targetGalleryList.href = `./gallery?title=${title}`;
-    });
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("gallery__list__image");
+  const image = document.createElement("img");
+  image.src = ImageID[0] ? `https://drive.google.com/uc?export=view&id=${ImageID[0]}` : "./images/empty.png";
+  image.alt = Title;
+
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("gallery__list__info");
+  const title = document.createElement("div");
+  title.classList.add("gallery__list__title");
+  title.textContent = Title;
+  const date = document.createElement("div");
+  date.classList.add("gallery__list__date");
+  date.textContent = Date;
+
+  infoContainer.appendChild(title);
+  infoContainer.appendChild(date);
+
+  imageContainer.appendChild(image);
+
+  galleryList.appendChild(imageContainer);
+  galleryList.appendChild(infoContainer);
+
+  galleryContainer.appendChild(galleryList);
+};
+
+const drawGalleryDetail = (gallery) => {
+  let { ImageID, Title, Text, Date } = gallery;
+  ImageID = ImageID.split(",");
+
+  const boardTitle = document.createElement("h2");
+  boardTitle.classList.add("board__list__title");
+  boardTitle.classList.add("font-bold");
+  boardTitle.textContent = Title;
+
+  const boardDate = document.createElement("div");
+  boardDate.classList.add("board__list__date");
+  boardDate.textContent = Date;
+
+  const boardContents = document.createElement("div");
+  boardContents.classList.add("board__list__contents");
+  ImageID.forEach((imageID, index) => {
+    const image = document.createElement("img");
+    image.src = imageID ? `https://drive.google.com/uc?export=view&id=${imageID}` : "./images/empty.png";
+    image.alt = `${Title} ${index + 1}`;
+    boardContents.appendChild(image);
   });
-}
+  const contents = document.createElement("p");
+  contents.innerHTML = Text;
+  boardContents.appendChild(contents);
+
+  boardContainer.appendChild(boardTitle);
+  boardContainer.appendChild(boardDate);
+  boardContainer.appendChild(boardContents);
+};
+
+getExcelData(sheetName, (gallerys) => {
+  // 갤러리
+  if (galleryId) {
+    boardContainer.style.display = "block";
+    galleryContainer.style.display = "none";
+
+    const result = gallerys.find((gallery) => gallery.Title.replace(/\s+/g, "-") === galleryId);
+    drawGalleryDetail(result);
+  }
+  // 갤러리 리스트
+  else {
+    gallerys.forEach((gallery) => {
+      drawGallerys(gallery);
+    });
+  }
+});
