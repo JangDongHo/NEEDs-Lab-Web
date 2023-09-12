379962dfd4a8ffecc7ead9ed136ea20664cfb134
Jang-DongHo <donghoo18@naver.com>
2023-09-12 09:03:26 +0900

    [Fix] 로고 안보이는 오류 수정

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import "./assets/scripts/main";
 
 // Images
 import "./assets/images/logo(white).png";
+import "./assets/images/logo(black).png";
 import "./assets/images/representative_image.jpg";
 import "./assets/images/professor.png";
